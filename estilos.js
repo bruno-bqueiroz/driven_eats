83bf3f1e3c422879b2d32de003928740be9ff2df
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-06-13 16:56:57 -0400

    versao final

---
+++ estilos.js
@@ -0,0 +1,36 @@
+function clicadoa(elemento) {
+    let cont = 0;
+    const botao = document.querySelector(".borda");
+    if(botao !== null){
+        botao.classList.remove("borda");
+    }
+    elemento.classList.add("borda");
+    cont++;
+
+    
+  }
+
+
+
+  function clicadob(elemento) {
+    let cont = 0;
+    const botao = document.querySelector(".borda");
+    if(botao !== null){
+        botao.classList.remove("borda");
+    }
+    elemento.classList.add("borda");
+    cont++;
+  }
+  
+
+
+
+  function clicadoc(elemento) {
+    let cont = 0;
+    const botao = document.querySelector(".borda");
+    if(botao !== null){
+        botao.classList.remove("borda");
+    }
+    elemento.classList.add("borda");
+    cont++;
+}
